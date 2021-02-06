f9e7801a05819d57b94cc5baf3d240685a282a99
GermanCHLB <75626046+GermanCHLB@users.noreply.github.com>
2021-02-06 21:38:16 +0500

    -

--- MainPage.js
+++ MainPage.js
@@ -1,8 +1,2 @@
-let butt = document.getElementById("button_plus");
-butt.onclick = () =>{
-    let a = parseInt(document.getElementById("first_num_input").value, 10);
-    let b = parseInt(document.getElementById("second_num_input").value, 10);
-    let answer = document.getElementById("answer");
-    console.log(answer.value = a+b);
-}
+
 
