e9bfa3f9199adaf496958525b88ac8491f44cbd5
GermanCHLB <75626046+GermanCHLB@users.noreply.github.com>
2021-02-06 23:48:12 +0500

    -

--- MainPage.js
+++ MainPage.js
@@ -1,2 +1,24 @@
-
-
+//#region Объявляем переменные
+let s = document.querySelector('.TextBox').value;
+let buttonIs = document.querySelector('.ButtonIs');
+let CLRButton = document.querySelector('.CLRButton');
+let DELButton = document.querySelector('.DELButtonIs');
+let DivisionButton = document.querySelector('.DivisionButton');
+let MultiplicationButton = document.querySelector('.MultiplicationButton');
+let SubtractionButton = document.querySelector('.SubtractionButton');
+let AdditionButton = document.querySelector('.AdditionButton');
+let LeftBracket = document.querySelector('.LeftBracket');
+let RightBracket = document.querySelector('.RightBracket');
+let Num1 = document.querySelector('.Num1');
+let Num2 = document.querySelector('.Num2');
+let Num3 = document.querySelector('.Num3');
+let Num4 = document.querySelector('.Num4');
+let Num5 = document.querySelector('.Num5');
+let Num6 = document.querySelector('.Num6');
+let Num7 = document.querySelector('.Num7');
+let Num8 = document.querySelector('.Num8');
+let Num9 = document.querySelector('.Num9');
+let Num0 = document.querySelector('.Num0');
+let Num00 = document.querySelector('.Num00');
+let Point = document.querySelector('.Point');
+//#endregion 
